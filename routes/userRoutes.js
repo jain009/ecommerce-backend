635af0155457b9df1fe013904a62d8affd910adf
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-05-14 02:14:45 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -22,7 +22,7 @@ router.route("/")
   router.post('/logout', logoutUser);
 router.post('/auth', authUser);
 router.route('/profile').get( protect, getUserProfile).put(protect , updateUserProfile);
-router.route('/:id').delete(protect , admin , deleteUser).get(protect,admin,getUsersByID).put(protect,admin,updateUser);
+router.route('/:id').delete(protect , admin , deleteUser).get(admin,getUsersByID).put(protect,admin,updateUser);
 
 
 
