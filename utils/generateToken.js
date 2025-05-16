d4c8d836dd8859d965c21f25207de73069af0406
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-05-17 01:53:00 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -5,13 +5,12 @@ const generateToken = (res, userId) => {
     expiresIn: '30d',
   });
 
- 
-res.cookie('jwt', token, {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: 'strict',
-  maxAge: 30 * 24 * 60 * 60 * 1000,
-   });
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
 };
 
 export default generateToken;
